ec63e394352e92deb090be6f567b953769f4a971
Rayane Bezerra <rayane.silva@ccc.ufcg.edu.br>
2023-06-02 22:18:37 -0300

    concertando erro do menu

--- main.pl
+++ main.pl
@@ -1,28 +1,19 @@
-:- use_module(library(system)).
-:- use_module(library(random)).
-
 % definição dos tipos de dados
-type Jogadores = [Jogador].
-type Nome = string.
-type Pontuacao = int.
-type Vez = int.
-type Tabuleiro = [[char]].
-data Jogador = Jogador Nome Pontuacao
-                    deriving (Show, Read).
-
+:- dynamic jogador/2. % dynamic para permitir a modificação em tempo de execução
+:- dynamic tabuleiro/1.
 
 % função que inicia o programa
 main :-
-    exibirConteudoArquivo('introducao.txt').
-    inicio_apresentacao([]),nl,
-    halt.
+    exibirConteudoArquivo('introducao.txt'),
+    sleep(3), % aguarda 5 segundos
+    menu([]).
 
 % ---------- INTRODUÇÃO E HISTÓRIA ----------
 
 exibirConteudoArquivo(NomeArquivo) :-
     open(NomeArquivo, read, Stream),
     repeat,
-    read_line_to_codes(Stream, Conteudo),
+    read_line_to_string(Stream, Conteudo),
     (   Conteudo \= end_of_file
     ->  format('~s~n', [Conteudo]),
         fail
@@ -30,46 +21,11 @@ exibirConteudoArquivo(NomeArquivo) :-
     ),
     close(Stream).
 
-
-% Função que apresenta uma introdução ao jogo e chama o menu
-inicio_apresentacao(Dados) :-
-    imprimiIntroducao,
-    write('\n\nPressione <Enter> para continuar'),
-    read_line(_),
-    menu(Dados).
-
-% função que irá imprimir a introdução do jogo
-imprimiIntroducao :-
-    system('clear'), % limpa a tela
-    open('introducao.txt', read, Handle),
-    imprimiTextoLentamente(Handle),
-    close(Handle).
-
-% função que irá imprimir a história do jogo
-imprimiHistoria(Dados) :-
-    system('clear'), % limpa a tela
-    open('historia.txt', read, Handle),
-    imprimiTextoLentamente(Handle),
-    close(Handle),
-    menu(Dados).
-
-% função para imprimir lentamente a introdução do jogo
-imprimiTextoLentamente(Handle) :-
-    read_line(Handle, Texto),
-    imprimiTextoLentamenteAux(Texto, Handle).
-
-imprimiTextoLentamenteAux([], _) :- !.
-imprimiTextoLentamenteAux([Char|Texto], Handle) :-
-    put_code(Char),
-    sleep(0.05), % aguarda 50ms
-    imprimiTextoLentamenteAux(Texto, Handle).
-
-
 % ---------- MENU ----------
 
 % função que exibe o Menu
 menu(Dados) :-
-    system('clear'), % limpa a tela
+    shell(clear), % limpa a tela
     writeln('-------------------- Batalha Naval --------------------'),
     writeln('\n ● Digite 1 para cadastrar jogador'),
     writeln('\n ● Digite 2 para jogar'),
@@ -77,7 +33,7 @@ menu(Dados) :-
     writeln('\n ● Digite 4 para ver o modo história'),
     writeln('\n ● Digite 0 para sair '),
     write('\n→ Opção: '),
-    read_line(Op),
+    read_line_to_string(user_input, Op),
     executarOpcao(Dados, Op).
 
 % função para manipular a opção escolhida pelo usuário
